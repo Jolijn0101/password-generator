dbe1eec4d906b6b51deaf16690c91b9848021bff
Jolijn Rosendaal <84015050+Jolijn0101@users.noreply.github.com>
2022-05-30 11:03:02 +0200

    password generator working

--- password-script.js
+++ password-script.js
@@ -3,28 +3,60 @@ let optionsArr = [];
 let password_arr = [];
 
 function generatePassword() {
-  for (let i = 0; i < size; i++) {
-    password_arr.push('*');
-  }
+  password_arr = new Array(size).fill('*', 0, size);
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  const special_characters = '!@#$%^&';
+  let newIndex;
 
-  for (let i = 0; i < optionsArr.length; i++) {
-    switch (optionsArr[i]) {
+  function switcher(option, index) {
+    switch (option) {
       case 'numbers':
-        console.log('doet ie het');
-        /*console.log(
-          password_arr[Math.floor(Math.random() * password_arr.length)]
-        );
-        console.log(Math.floor(Math.random() * 10));*/
+        password_arr[index] = Math.floor(Math.random() * 9);
         break;
-
+      case 'uppercase_letters':
+        password_arr[index] =
+          alphabet[Math.floor(Math.random() * alphabet.length)].toUpperCase();
+        break;
+      case 'lowercase_letters':
+        password_arr[index] =
+          alphabet[Math.floor(Math.random() * alphabet.length)];
+        break;
+      case 'special_characters':
+        password_arr[index] =
+          special_characters[
+            Math.floor(Math.random() * special_characters.length)
+          ];
+        break;
       default:
         break;
     }
   }
+
+  // place first obligated elements
+  for (let i = 0; i < optionsArr.length; i++) {
+    newIndex = Math.floor(Math.random() * password_arr.length);
+
+    if (password_arr[newIndex] !== '*') {
+      newIndex = password_arr.indexOf('*');
+    }
+    switcher(optionsArr[i], newIndex);
+  }
+
+  // fill empty spaces with random values
+  for (let i = 0; i < password_arr.length; i++) {
+    if (password_arr[i] === '*') {
+      switcher(optionsArr[Math.floor(Math.random() * optionsArr.length)], i);
+    }
+  }
+  console.log(password_arr);
+  document.querySelector('#newPassword').innerHTML =
+    'Password: ' + password_arr.join('');
+  document.querySelector('button').innerHTML = 'regenerate';
+  clearOptions();
 }
 
 function handleSubmit() {
-  size = document.getElementById('password_length').value;
+  size = parseFloat(document.getElementById('password_length').value);
 
   let checkboxes = document.getElementsByName('options');
   for (let i = 0; i < checkboxes.length; i++) {
@@ -32,6 +64,13 @@ function handleSubmit() {
       optionsArr.push(checkboxes[i].value);
     }
   }
+
+  if (optionsArr.length === 0) {
+    alert('choose a password option');
+    clearOptions();
+    return null;
+  }
+
   generatePassword();
 }
 
